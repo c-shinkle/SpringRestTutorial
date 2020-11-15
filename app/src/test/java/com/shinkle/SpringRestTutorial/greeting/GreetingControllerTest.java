c9a08a96d4d92c4b754b92cd9abefaf4670addec
c-shinkle <cshinkle@iastate.edu>
2020-11-15 14:29:10 -0600

    Properly test drove greeting controller

--- app/src/test/java/com/shinkle/SpringRestTutorial/greeting/GreetingControllerTest.java
+++ app/src/test/java/com/shinkle/SpringRestTutorial/greeting/GreetingControllerTest.java
@@ -7,10 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GreetingController.class)
@@ -20,11 +21,37 @@ class GreetingControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void shouldIncrementCounterWhenCalled() throws Exception {
+    void shouldGetUserWhenCalled() throws Exception {
         mockMvc.perform(get("/greeting")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("id").value("1"))
-                .andDo(print());
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("content").exists());
+    }
+
+    @Test
+    void shouldAddNameToGreeting() throws Exception {
+        mockMvc.perform(get("/greeting")
+                .param("name", "Lucas")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("content").exists())
+                .andExpect(jsonPath("content").value("Hello, Lucas!"));
+    }
+
+    @Test
+    void shouldIncrementCounterForEachCall() throws Exception {
+        ResultActions firstCall = mockMvc.perform(get("/greeting").contentType(MediaType.APPLICATION_JSON));
+        ResultActions secondCall = mockMvc.perform(get("/greeting").contentType(MediaType.APPLICATION_JSON));
+
+        firstCall
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("id").value("1"));
+
+        secondCall
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("id").value("2"));
     }
 }
