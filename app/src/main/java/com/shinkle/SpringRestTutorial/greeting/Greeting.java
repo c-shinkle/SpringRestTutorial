98533f1a62e1c5f3a13551803588cd93c189c895
c-shinkle <cshinkle@iastate.edu>
2020-11-22 20:36:11 -0600

    started over again. I realized by build issue was related to IntelliJ, but there was still an annoying wrinkle in the directory layout that I wanted to resolve

--- app/src/main/java/com/shinkle/SpringRestTutorial/greeting/Greeting.java
+++
@@ -1,9 +0,0 @@
-package com.shinkle.SpringRestTutorial.greeting;
-
-import lombok.Value;
-
-@Value
-public class Greeting {
-    long id;
-    String content;
-}
--- app/src/main/java/com/shinkle/SpringRestTutorial/user/User.java
+++
@@ -1,9 +0,0 @@
-package com.shinkle.SpringRestTutorial.user;
-
-import lombok.Value;
-
-@Value
-public class User {
-    int id;
-    String name;
-}
